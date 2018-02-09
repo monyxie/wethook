8b5e5f17f2a037706115fa7346e1aaa8c960d82e
Mony Xie <monyxie@gmail.com>
2018-02-09 17:12:14 +0800

    forward.php.sample -> forward.php

--- forward.php
+++ forward.php
@@ -1,6 +1,10 @@
 <?php
 $url = 'http://localhost:33133/';
 $post_data = file_get_contents('php://input');
+$headers = [];
+foreach (getallheaders() as $key => $val) {
+    $headers = "$key: $val";
+}
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_TIMEOUT, 3);
@@ -9,7 +13,7 @@ curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_FRESH_CONNECT, 1);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
-curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json']);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 $response = curl_exec($ch);
 
 echo $response;
