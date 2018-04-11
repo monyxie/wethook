cddfc6e7937b06d06546492011cdd17fcf974499
Mony Xie <monyxie@gmail.com>
2018-04-11 17:12:13 +0800

    Support headers in forward script

--- forward.php
+++ forward.php
@@ -1,14 +1,32 @@
 <?php
-$url = 'http://localhost:33133/';
-$post_data = file_get_contents('php://input');
-$headers = [];
+$forward_url = 'http://localhost:33133/';
+
+if (!function_exists('getallheaders'))  {
+    function getallheaders()
+    {
+        if (!is_array($_SERVER)) {
+            return array();
+        }
+
+        $headers = array();
+        foreach ($_SERVER as $name => $value) {
+            if (substr($name, 0, 5) == 'HTTP_') {
+                $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+            }
+        }
+        return $headers;
+    }
+}
+
+$post_data   = file_get_contents('php://input');
+$headers     = [];
 foreach (getallheaders() as $key => $val) {
-    $headers = "$key: $val";
+    $headers []= "$key: $val";
 }
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_TIMEOUT, 3);
-curl_setopt($ch, CURLOPT_URL, $url);
+curl_setopt($ch, CURLOPT_URL, $forward_url);
 curl_setopt($ch, CURLOPT_POST, 1);
 curl_setopt($ch, CURLOPT_FRESH_CONNECT, 1);
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
